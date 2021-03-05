50facbb72f02286d8c59a5447fd234d0780b3ac4
spettigrew <sara.pettigrew5@gmail.com>
2021-03-04 20:56:25 -0700

    Server running with request and response

--- reference/http_demo.js
+++ reference/http_demo.js
@@ -1,1 +1,10 @@
 // Gets a server running 
+
+const http = require('http');
+http.createServer((req, res) => {
+    // Send a request and get a response
+    res.write("Hello World");
+    res.end()
+
+})
+.listen(5000, () => console.log("Server running..."));
